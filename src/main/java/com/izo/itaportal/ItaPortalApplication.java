bba451236ca62a39e8a27f1a3c25cd766f28ce26
SDM2552 <se299792458@gmail.com>
2024-05-13 14:47:24 +0900

    'init'

---
+++ src/main/java/com/izo/itaportal/ItaPortalApplication.java
@@ -0,0 +1,13 @@
+package com.izo.itaportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItaPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItaPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/izo/itaportal/ItaPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izo.itaportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItaPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
