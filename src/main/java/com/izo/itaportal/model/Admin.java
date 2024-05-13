d7acc1960a5f92013aa8cc9dc9e68d3378a5a157
으악이 <155132099+seonhoho@users.noreply.github.com>
2024-05-13 17:15:39 +0900

    Admin model

---
+++ src/main/java/com/izo/itaportal/model/Admin.java
@@ -0,0 +1,16 @@
+package com.izo.itaportal.model;
+
+import lombok.Data;
+
+@Data
+public class Admin {
+
+    private Long idAdmin;
+    private String name;
+    private String birth;
+    private String tel;
+    private String address;
+    private Long idUser;
+    private String gender;
+    private Long idUser2;
+}
