03fb56494e2145df36c6d4f70a008c897dc5ab47
idnodea <59271963+idnodea@users.noreply.github.com>
2024-05-14 17:05:59 +0900

    공지사항관련

--- src/main/java/com/izo/itaportal/entity/Enrollment.java
+++ src/main/java/com/izo/itaportal/entity/Enrollment.java
@@ -15,9 +15,10 @@ public class Enrollment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idEnrollment; // 수강 신청 ID
 
-    private int idStudent;    // 학생 ID
+    private int idStudient;    // 학생 ID
     private String pgmName;  //프로그램 이름
     private int idProgram;    // 프로그램 ID
+    private int idCate;    //  카테고리ID
     private String stDt;    // 수강 신청 날짜
     private String status;    // 수강 신청 상태 (예: "확인중", "승인", "취소")
 
