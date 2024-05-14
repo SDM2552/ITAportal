a17c19aa298dd461930f6438a0e2ccd9b717f8b3
으악이 <155132099+seonhoho@users.noreply.github.com>
2024-05-14 17:46:37 +0900

    회원가입 중간 업로드

---
+++ src/main/java/com/izo/itaportal/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.izo.itaportal.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("signUp1")
+    public String signUpSun(){
+        return "member/signUpSun";
+    }
+
+    @PostMapping("signUp2")
+    public String signUp(@RequestParam String role, Model model){
+            model.addAttribute("role",role);
+//        System.out.println(role);
+        return "member/signUp";
+    }
+
+    @PostMapping("signUpCheck")
+    public String signUpCheck(){
+        if(role == "stu") {
+
+        } else if (role == "prof") {
+
+        }
+        return "";
+    }
+
+}
