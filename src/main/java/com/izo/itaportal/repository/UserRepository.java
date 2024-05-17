a201b92cefdd3207ceab1e795d4123c44a3751df
으악이 <155132099+seonhoho@users.noreply.github.com>
2024-05-17 09:38:44 +0900

    user

--- src/main/java/com/izo/itaportal/repository/UserRepository.java
+++ src/main/java/com/izo/itaportal/repository/UserRepository.java
@@ -2,12 +2,14 @@ package com.izo.itaportal.repository;
 
 import com.izo.itaportal.model.User;
 import org.apache.ibatis.annotations.Mapper;
-
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserRepository {
+    String getPasswordByLoginId(String LoginId);
 
-
+    @Select("select * from User where login_id = #{loginId}")
+    User findByLoginId(String loginId); //이미 가입한 ID인지 중복 체크용
 
     void insertUser(User user); // 신규 유저 등록
 
