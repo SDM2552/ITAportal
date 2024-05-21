2f9afec2c9ef870670a30831974002153b87439f
zxv5503 <zxv5503@naver.com>
2024-05-21 15:53:58 +0900

    jiung

--- src/main/java/com/izo/itaportal/repository/ProgramRepository.java
+++ src/main/java/com/izo/itaportal/repository/ProgramRepository.java
@@ -13,15 +13,20 @@ import java.util.List;
 @Mapper
 public interface ProgramRepository {
     public List<Program> selectProgram();
+
     public Program selectProgramById(int idPgm);
+
     public int insert(Program program);
 
     public int update(Program program);
 
     public int delete(int idPgm);
+
     //강사용 강의리스트 조회 쿼리 @김봄이
     public List<Program> selectPgmForProf(int idProf);
+
     public List<ProgramAllDto> selectAllPgm(int idProf);
+
     ProgramAllDto selectPgmDetail(@Param("idProf") int idProf, @Param("idPgm") int idPgm);
 
 
